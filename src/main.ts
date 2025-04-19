1f01a0a4e475ce2ffc4526bf990481d58f321b52
zy1p <zy1p@icloud.com>
2025-04-19 15:00:13 -0400

    chore: add swagger

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,21 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('API document')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(3000);
 }
