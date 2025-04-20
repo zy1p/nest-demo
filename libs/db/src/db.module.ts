4db6341827675995823ccedba6be56b98c3583dd
zy1p <zy1p@icloud.com>
2025-04-19 23:21:11 -0400

    chore: progress cooking db module

--- libs/db/src/db.module.ts
+++ libs/db/src/db.module.ts
@@ -1,9 +1,28 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module, Provider } from '@nestjs/common';
 
+import { QUERY_CLIENT } from '.';
 import { DbService } from './db.service';
 
 @Module({
   providers: [DbService],
   exports: [DbService],
 })
-export class DbModule {}
+export class DbModule {
+  static forRoot(): DynamicModule {
+    const providers: Provider[] = [
+      {
+        provide: QUERY_CLIENT,
+        useFactory: () => {
+          // TODO: implement the query client
+        },
+      },
+    ];
+
+    return {
+      module: DbModule,
+      global: true,
+      providers,
+      exports: providers,
+    };
+  }
+}
