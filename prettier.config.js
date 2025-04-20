ea8680494fbc859d8a2d7978d8a26c459fd17071
zy1p <zy1p@icloud.com>
2025-04-19 22:27:10 -0400

    ci: add prettier import sort

---
+++ prettier.config.js
@@ -0,0 +1,21 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  importOrder: [
+    '<TYPES>',
+    '^@nestjs/(.*)$',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^[.]',
+  ],
+  importOrderParserPlugins: ['typescript', 'decorators-legacy', ''],
+  importOrderTypeScriptVersion: '5.0.0',
+  importOrderCaseSensitive: false,
+};
+
+module.exports = config;
