e4771c1751787eb35c04efa01ec82ae4e075db17
zy1p <zy1p@icloud.com>
2025-04-22 18:34:50 -0400

    chore: update sign in dto constraint

--- libs/auth/src/dtos/sigin-in.dto.ts
+++ libs/auth/src/dtos/sigin-in.dto.ts
@@ -2,7 +2,7 @@ import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
 export const signInSchema = z.object({
-  emailOrUsername: z.string().email().or(z.string()),
+  emailOrUsername: z.string().email().or(z.string().min(3)),
   password: z.string().min(8),
 });
 
